c420069f9e81bd6c59496eaa7df1489ab132b0e3
anhductn2001 <ntd1312000@gmail.com>
2024-03-11 22:56:32 +0700

    debug

--- relayer/docker.go
+++ relayer/docker.go
@@ -20,7 +20,7 @@ import (
 )
 
 const (
-	defaultRlyHomeDirectory = "/home/relayer"
+	defaultRlyHomeDirectory = "/tmp/rly"
 )
 
 // DockerRelayer provides a common base for relayer implementations
@@ -456,7 +456,7 @@ func (r *DockerRelayer) Name() string {
 
 // Bind returns the home folder bind point for running the node.
 func (r *DockerRelayer) Bind() []string {
-	return []string{"/tmp:/home"}
+	return []string{"/tmp/rly" + ":" + r.HomeDir()}
 }
 
 // HomeDir returns the home directory of the relayer on the underlying Docker container's filesystem.

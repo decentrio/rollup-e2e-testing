ff7cd18417394c2ddf161fb3c2263e3f34f40a60
anhductn2001 <ntd1312000@gmail.com>
2024-03-11 12:44:38 +0700

    bind

--- relayer/docker.go
+++ relayer/docker.go
@@ -496,7 +496,7 @@ func (r *DockerRelayer) Name() string {
 
 // Bind returns the home folder bind point for running the node.
 func (r *DockerRelayer) Bind() []string {
-	return []string{r.volumeName + ":" + r.HomeDir()}
+	return []string{"/tmp/" + r.volumeName + ":" + r.HomeDir()}
 }
 
 // HomeDir returns the home directory of the relayer on the underlying Docker container's filesystem.

8992dfbfcbd651c09879b2ec035b0a1b44903963
Piotr Gesicki <piotr.gesicki@gmail.com>
2024-03-15 16:40:15 +0100

    add wait function

--- dymension/utils.go
+++ dymension/utils.go
@@ -1,9 +1,13 @@
 package dymension
 
 import (
+	"context"
+	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/decentrio/rollup-e2e-testing/blockdb"
+	"github.com/decentrio/rollup-e2e-testing/dymension"
 )
 
 func MapToEibcEvent(event blockdb.Event) (EibcEvent, error) {
@@ -29,4 +33,44 @@ func MapToEibcEvent(event blockdb.Event) (EibcEvent, error) {
 	}
 
 	return eibcEvent, nil
+}
+
+func WaitUntilRollappHeightIsFinalized(ctx context.Context, dymension *dymension.DymHub, rollappChainID string, targetHeight uint64, timeoutSecs int) (bool, error) {
+	startTime := time.Now()
+	timeout := time.Duration(timeoutSecs) * time.Second
+
+	for {
+		select {
+		case <-ctx.Done():
+			return false, ctx.Err()
+		case <-time.After(timeout):
+			return false, fmt.Errorf("specified rollapp height %d not found within the timeout", targetHeight)
+		default:
+			rollappState, err := dymension.QueryRollappState(ctx, rollappChainID, true)
+			if err != nil {
+				if time.Since(startTime) < timeout {
+					time.Sleep(2 * time.Second) // Wait for 2 seconds before retrying.
+					continue                    // Retry the loop.
+				} else {
+					return false, fmt.Errorf("error querying rollapp state: %v", err)
+				}
+			}
+
+			for _, bd := range rollappState.StateInfo.BlockDescriptors.BD {
+				height, err := strconv.ParseUint(bd.Height, 10, 64)
+				if err != nil {
+					continue
+				}
+				if height == targetHeight {
+					return true, nil
+				}
+			}
+
+			if time.Since(startTime)+2*time.Second < timeout {
+				time.Sleep(2 * time.Second)
+			} else {
+				return false, fmt.Errorf("specified rollapp height %d not found within the timeout", targetHeight)
+			}
+		}
+	}
 }

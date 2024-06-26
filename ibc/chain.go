c2a85546849efacea7cd4ab3284d5941c17c8e14
ThanhNhann <thanhnhan98qh@gmail.com>
2024-06-26 13:21:49 +0700

    update logic configurate

--- ibc/chain.go
+++ ibc/chain.go
@@ -112,8 +112,6 @@ type Hub interface {
 type RollApp interface {
 	// Configuration sets up everything needed (validators, gentx, fullnodes, peering, additional accounts) for Rollapp from genesis.
 	Configuration(testName string, ctx context.Context, forkRollAppId string, gensisContent []byte, additionalGenesisWallets ...WalletData) error
-	// Configuration sets up incase we want to set up a roll app with an existing genesis
-	ConfigurationWithGenesisFile(testName string, ctx context.Context, genesisContent []byte) error
 	// Get key sequencer location
 	GetSequencerKeyDir() string
 	// Show Sequencer Key

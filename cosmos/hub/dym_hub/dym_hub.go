96c3aea7b3f81384cc34c2632b91ca8ec6095049
anhductn2001 <ntd1312000@gmail.com>
2024-07-01 14:09:06 +0700

    add flag transfers-enabled

--- cosmos/hub/dym_hub/dym_hub.go
+++ cosmos/hub/dym_hub/dym_hub.go
@@ -313,6 +313,7 @@ func (c *DymHub) Start(testName string, ctx context.Context, additionalGenesisWa
 
 		// hasFlagGenesisPath, ok := c.extraFlags["genesis-accounts-path"].(bool)
 		flags := map[string]string{}
+		flags["transfers-enabled"] = "true"
 		// if hasFlagGenesisPath && ok {
 		// 	flags["genesis-accounts-path"] = validator0.HomeDir() + "/" + rollAppChainID + "_genesis_accounts.json"
 		// }
@@ -429,6 +430,7 @@ func (c *DymHub) SetupRollAppWithExitsHub(ctx context.Context) error {
 
 		// hasFlagGenesisPath, ok := c.extraFlags["genesis-accounts-path"].(bool)
 		flags := map[string]string{}
+		flags["transfers-enabled"] = "true"
 		// if hasFlagGenesisPath && ok {
 		// 	flags["genesis-accounts-path"] = validator0.HomeDir() + "/" + rollAppChainID + "_genesis_accounts.json"
 		// }

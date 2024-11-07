0ab2fee3053fa98e6e447d3584e5516a6cbfc8ae
anhductn2001 <ntd1312000@gmail.com>
2024-11-07 10:09:35 +0700

    update QueryPendingPacketsByAddress

--- cosmos/node.go
+++ cosmos/node.go
@@ -1928,9 +1928,9 @@ func (node *Node) FinalizePacket(ctx context.Context, keyName, rollappID, proofH
 	return node.ExecTx(ctx, keyName, command...)
 }
 
-func (node *Node) QueryPendingPacketsByAddress(ctx context.Context, rollappID, receiver string) (QueryPendingPacketByReceiverListResponse, error) {
+func (node *Node) QueryPendingPacketsByAddress(ctx context.Context, addr string) (QueryPendingPacketByReceiverListResponse, error) {
 	command := []string{
-		"delayedack", "pending-packets-by-address", rollappID, receiver,
+		"delayedack", "pending-packets-by-address", addr,
 	}
 	stdout, _, err := node.ExecQuery(ctx, command...)
 

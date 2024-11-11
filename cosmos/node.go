79af03fb513e0615ce729fad8c2aa0b89ccfd924
anhductn2001 <ntd1312000@gmail.com>
2024-11-11 22:42:39 +0700

    update init supply

--- cosmos/node.go
+++ cosmos/node.go
@@ -789,7 +789,7 @@ func (node *Node) RegisterRollAppToHub(ctx context.Context, keyName, bech32, rol
 		command = append(
 			command, "rollapp", "create-rollapp",
 			rollappChainID, string(alias), vmtype, "--bech32-prefix", bech32Prefix, "--init-sequencer", sequencerAddr, "--genesis-checksum", checksum, "--metadata", keyDir+"/metadata.json", "--genesis-accounts", bech32+":"+dymension.GenesisEventAmount.String(),
-			"--native-denom", keyDir+"/native_denom.json", "--initial-supply", "100000000010100000000000000000000",
+			"--native-denom", keyDir+"/native_denom.json", "--initial-supply", "100000010000100000000000000000000",
 			"--broadcast-mode", "async", "--keyring-dir", keyPath)
 	} else {
 		vmtype = "WASM"

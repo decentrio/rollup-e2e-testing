8778fba0b28132a11ef98dcfa24325740265e799
anhductn2001 <ntd1312000@gmail.com>
2024-11-12 08:21:13 +0700

    provided fee < minimum global fee

--- cosmos/node.go
+++ cosmos/node.go
@@ -759,7 +759,7 @@ func (node *Node) Gentx(ctx context.Context, name string, genesisSelfDelegation 
 		command = append(command, "gentx", valKey, fmt.Sprintf("%s%s", genesisSelfDelegation.Amount.String(), genesisSelfDelegation.Denom),
 			"--keyring-backend", keyring.BackendTest,
 			"--chain-id", node.Chain.Config().ChainID,
-			"--fees", fmt.Sprintf("4000000000000%s", node.Chain.Config().Denom),
+			"--fees", fmt.Sprintf("4000000000000000%s", node.Chain.Config().Denom),
 		)
 	} else {
 		command = append(command, "gentx", valKey, fmt.Sprintf("%s%s", genesisSelfDelegation.Amount.String(), genesisSelfDelegation.Denom),

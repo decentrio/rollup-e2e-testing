ca2e32d7cd8d686b4fb4d01c9766352d8df72895
hungdinh82 <hungngohihihi@gmail.com>
2024-11-12 21:05:46 +0700

    fix flag submitFraudProposal

--- cosmos/node.go
+++ cosmos/node.go
@@ -1370,7 +1370,7 @@ func (node *Node) SubmitFraudProposal(ctx context.Context, keyName, rollappId, h
 
 	var command []string
 	command = append(command, "gov", "submit-proposal", "submit-fraud-proposal",
-		"--message", string(messageBytes), "--gas", "auto", "--broadcast-mode", "async", "--deposit", deposit)
+		"--title=fraud", string(messageBytes), "--gas", "auto", "--broadcast-mode", "async", "--deposit", deposit)
 
 	return node.ExecTx(ctx, keyName, command...)
 }

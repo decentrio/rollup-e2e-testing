73cfd9a82257e888876b22949e0293bb4b25ce77
ThanhNhann <thanhnhan98qh@gmail.com>
2024-11-22 16:20:26 +0700

    update GrantAuthorization

--- cosmos/node.go
+++ cosmos/node.go
@@ -990,7 +990,7 @@ func (node *Node) CreateGroupPolicy(ctx context.Context, keyName, metadata, poli
 
 func (node *Node) GrantAuthorization(ctx context.Context, keyName, policyAddress, spendLimit, rollappID, denoms, minLPFeePercentage, maxPrice, feeShare string) (string, error) {
 	var command []string
-	command = append(command, "eibc", "grant", policyAddress, "--spend-limit", spendLimit, "--rollapps", rollappID, "--denoms", denoms, "--min-lp-fee-percentage", minLPFeePercentage, "--max-price", maxPrice, "--operator-fee-share", feeShare, "--settlement-validated")
+	command = append(command, "eibc", "grant", policyAddress, "--spend-limit", spendLimit, "--rollapp", rollappID, "--denoms", denoms, "--min-lp-fee-percentage", minLPFeePercentage, "--max-price", maxPrice, "--operator-fee-share", feeShare, "--settlement-validated")
 
 	hash, err := node.ExecTx(ctx, keyName, command...)
 	return hash, err

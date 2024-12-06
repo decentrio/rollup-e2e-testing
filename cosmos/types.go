000618f63c974fc7682cc7820fca646035e8b98e
hungdinh82 <hungngohihihi@gmail.com>
2024-12-06 15:32:07 +0700

    update logic

--- cosmos/types.go
+++ cosmos/types.go
@@ -258,11 +258,6 @@ type QueryRewardAddressResponse struct {
 	RewardAddr string `protobuf:"bytes,1,opt,name=reward_addr,json=rewardAddr,proto3" json:"reward_addr,omitempty"`
 }
 
-type MsgUpdateRewardAddress struct {
-	Creator string `protobuf:"bytes,1,opt,name=creator,proto3" json:"creator,omitempty"`
-	RewardAddr string `protobuf:"bytes,2,opt,name=reward_addr,json=rewardAddr,proto3" json:"reward_addr,omitempty"`
-}
-
 // Sequencer defines a sequencer identified by its' address (sequencerAddress).
 // The sequencer could be attached to only one rollapp (rollappId).
 type Sequencer struct {

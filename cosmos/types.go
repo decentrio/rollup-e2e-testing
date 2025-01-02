80821e4b7bdcd6189b2ff837050e750adf752d9d
ThanhNhann <thanhnhan98qh@gmail.com>
2025-01-02 12:22:59 +0700

    minor

--- cosmos/types.go
+++ cosmos/types.go
@@ -501,5 +501,7 @@ type QueryContractsByCodeResponse struct {
 
 type QuerySmartContractStateResponse struct {
 	// Data contains the json data returned from the smart contract
-	Data string `protobuf:"bytes,1,opt,name=data,proto3,casttype=RawContractMessage" json:"data,omitempty"`
+	Data RawContractMessage `protobuf:"bytes,1,opt,name=data,proto3,casttype=RawContractMessage" json:"data,omitempty"`
 }
+
+type RawContractMessage string

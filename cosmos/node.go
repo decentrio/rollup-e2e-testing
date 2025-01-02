153862c517684c3264bf5e1636622b5d17999670
ThanhNhann <thanhnhan98qh@gmail.com>
2025-01-02 22:50:06 +0700

    udpate WasmInstantiateContract

--- cosmos/node.go
+++ cosmos/node.go
@@ -1019,9 +1019,9 @@ func (node *Node) WasmStore(ctx context.Context, keyName, path string) error {
 	return err
 }
 
-func (node *Node) WasmInstantiateContract(ctx context.Context, keyName, codeId, initCW20 string) error {
+func (node *Node) WasmInstantiateContract(ctx context.Context, keyName, codeId, initCW20, label string) error {
 	var command []string
-	command = append(command, "wasm", "instantiate", codeId, initCW20, "--label", "test", "--no-admin")
+	command = append(command, "wasm", "instantiate", codeId, initCW20, "--label", label, "--no-admin")
 
 	_, err := node.ExecTx(ctx, keyName, command...)
 	return err

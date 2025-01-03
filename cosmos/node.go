cfc3898c829bc64bc4e8c00922148da85ba39eb1
anhductn2001 <ntd1312000@gmail.com>
2025-01-03 09:46:08 +0700

    initial supply

--- cosmos/node.go
+++ cosmos/node.go
@@ -941,7 +941,7 @@ func (node *Node) RegisterRollAppToHubWithoutGenesisAccount(ctx context.Context,
 		command = append(
 			command, "rollapp", "create-rollapp",
 			rollappChainID, string(alias), vmtype, "--bech32-prefix", bech32Prefix, "--init-sequencer", sequencerAddr, "--genesis-checksum", checksum, "--metadata", keyDir+"/metadata.json", "--genesis-accounts", bech32+":"+dymension.GenesisEventAmount.String(),
-			"--native-denom", keyDir+"/native_denom.json", "--initial-supply", "10000200000000000000000000",
+			"--native-denom", keyDir+"/native_denom.json", "--initial-supply", "10000100000000000000000000",
 			"--broadcast-mode", "async", "--keyring-dir", keyPath)
 	}
 
